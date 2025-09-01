92d45d7514b77647f7cc5bd39e3467fa11749bc2
josea <josealdof25@gmail.com>
2025-09-01 01:53:50 -0600

    Agregar proyecto p03 con archivos PHP y XHTML

---
+++ practicas/p03/inclusion_archivos/pie.inc.php
@@ -0,0 +1,13 @@
+<?php
+echo <<<HTML
+<div>
+  <h1 style="border-width:3;border-style:groove; background-color:#ffcc99;">
+    Final de la página PHP Vínculos útiles :
+    <a href="php.net">php.net</a> &nbsp; <a href="mysql.org">mysql.org</a>
+  </h1>
+HTML;
+echo "Nombre del archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp;&nbsp;";
+echo "Nombre del archivo incluido: ", __FILE__ ,"</div>";
+?>
+</body>
+</html>
