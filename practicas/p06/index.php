34c517a613784ac23482ecdb2bae798b0ec64644
josea <josealdof25@gmail.com>
2025-09-17 14:13:06 -0600

    Ejercicio 1 COMPLETADO

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -3,27 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 6</title>
 </head>
 <body>
+    <?php include 'src/funciones.php'; ?>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-
-        if(isset($_GET['numero']))
-        {
+        if (isset($_GET['numero'])) {
             $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+            echo "<h3>" . esMultiplo57($num) . "</h3>";
         }
     ?>
-
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
         Name: <input type="text" name="name"><br>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,1 +1,10 @@
-
+<?php
+// Función que revisa si un número es múltiplo de 5 y 7
+function esMultiplo57($num) {
+    if ($num % 5 == 0 && $num % 7 == 0) {
+        return "R= El número $num SÍ es múltiplo de 5 y 7.";
+    } else {
+        return "R= El número $num NO es múltiplo de 5 y 7.";
+    }
+}
+?>
